97a6887ee2e7419c985cb463dd1c9f4092b3d242
DizzleReply <D.grimm@reply.de>
2022-05-18 16:01:10 +0200

    add sample function to symbolize heredity

--- contracts/inheritance.sol
+++ contracts/inheritance.sol
@@ -1,15 +1,5 @@
 contract Resource {
-    struct Resource {
-        uint256 resourceId;
-        string resourceName;
-        bool loanable;
-        uint256 loanInterval;
-        uint256 resourceElectricityCostPerHour;
-        uint256 resourceCosts;
-        uint256 computingPower;
-        bool buyOut;
-        uint256 appraisedValue;
-    }
+    function rentResource() public {}
 }
 
 contract Room is Resource {}
